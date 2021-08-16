21057150d3e30a5149f2aa9b8c388e8fcfd9d0f1
Joseph Lingenberg <lingenberg.joseph@gmail.com>
2021-08-16 19:17:40 +0300

    Added 'bill' payment method

--- app/routes/payment_routes.py
+++ app/routes/payment_routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import request, render_template
+import requests, json
+from flask import request, render_template, redirect
 from app.utils.utils import generate_sign
 from app.utils.secret import secret_key, shop_id, payway
 
@@ -16,7 +17,28 @@ def pay():
 
 @app.route("/bill", methods=["POST"])
 def bill():
-    pass
+    data = request.form.to_dict()
+    data["amount"] = format(float(data["amount"]),".2f")
+
+    sign = generate_sign(secret_key, shop_id=shop_id, shop_amount=data["amount"],
+        shop_currency=data["currency"], shop_order_id="1111", payer_currency=data["currency"])
+
+    headers = {"Content-Type": "application/json"}
+    payload = {
+        "shop_id": shop_id,
+        "shop_amount": data["amount"],
+        "shop_currency": data["currency"],
+        "payer_currency": data["currency"],
+        "sign": sign,
+        "shop_order_id": "1111",
+        "description": data["description"],
+        "payer_account": "201494711279"
+    }
+    session = requests.Session()
+    response = session.post("https://core.piastrix.com/bill/create", headers=headers, data=json.dumps(payload))
+    json_data = response.json()["data"]
+
+    return redirect(json_data["url"])
 
 @app.route("/invoice", methods=["POST"])
 def invoice():
