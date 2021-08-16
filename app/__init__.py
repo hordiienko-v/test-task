2f6f1f8dd30ce994bc25aa2cca36979198ff237d
Joseph Lingenberg <lingenberg.joseph@gmail.com>
2021-08-16 16:34:18 +0300

    Added routes for different payment methods and modified index.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+from flask import redirect
 import os
 
 app = Flask(__name__)
@@ -9,3 +10,15 @@ def home():
         return render_template("index.html")
     else:
         pass
+
+@app.route("/pay", methods=["POST"])
+def pay():
+    pass
+
+@app.route("/bill", methods=["POST"])
+def bill():
+    pass
+
+@app.route("/invoice", methods=["POST"])
+def invoice():
+    pass
