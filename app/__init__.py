2afdae7e5fcf8cd4766dcb1cfb017f5628950b78
Joseph Lingenberg <lingenberg.joseph@gmail.com>
2021-08-17 10:12:52 +0300

    Code refactoring

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,9 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def home():
     if request.method == "GET":
         return render_template("index.html")
-    else:
-        pass
 
 from app.routes import payment_routes
--- app/routes/payment_routes.py
+++ app/routes/payment_routes.py
@@ -6,71 +6,77 @@ from app.utils.secret import secret_key, shop_id, payway
 
 @app.route("/pay", methods=["POST"])
 def pay():
-    data = request.form.to_dict()
-    data["amount"] = format(float(data["amount"]),".2f")
+    amount = request.form.get("amount")
+    description = request.form.get("description")
+    currency = request.form.get("currency")
 
-    sign = generate_sign(secret_key, amount=data["amount"], currency=data["currency"],
-        shop_id=shop_id, shop_order_id="1111")
+    amount = format(float(amount),".2f")
+    sign = generate_sign(secret_key, amount=amount, currency=currency,
+        shop_id=shop_id, shop_order_id="1")
 
-    return render_template("pay.html", amount=data["amount"], currency=data["currency"],
-        shop_id=shop_id, shop_order_id="1111", sign=sign, description=data["description"])
+    return render_template("pay.html", amount=amount, currency=currency,
+        shop_id=shop_id, shop_order_id="1", sign=sign, description=description)
 
 @app.route("/bill", methods=["POST"])
 def bill():
-    data = request.form.to_dict()
-    data["amount"] = format(float(data["amount"]),".2f")
+    amount = request.form.get("amount")
+    description = request.form.get("description")
+    currency = request.form.get("currency")
 
-    sign = generate_sign(secret_key, shop_id=shop_id, shop_amount=data["amount"],
-        shop_currency=data["currency"], shop_order_id="1111", payer_currency=data["currency"])
+    amount = format(float(amount),".2f")
+
+    sign = generate_sign(secret_key, shop_amount=amount, shop_currency=currency,
+        shop_id=shop_id, shop_order_id="1", payer_currency=currency)
 
     headers = {"Content-Type": "application/json"}
     payload = {
         "shop_id": shop_id,
-        "shop_amount": data["amount"],
-        "shop_currency": data["currency"],
-        "payer_currency": data["currency"],
+        "shop_amount": amount,
+        "shop_currency": currency,
+        "payer_currency": currency,
         "sign": sign,
-        "shop_order_id": "1111",
-        "description": data["description"],
+        "shop_order_id": "1",
+        "description": description,
         "payer_account": "201494711279"
     }
     session = requests.Session()
     response = session.post("https://core.piastrix.com/bill/create", headers=headers, data=json.dumps(payload))
-    json_data = response.json()["data"]
 
-    return redirect(json_data["url"])
+    if response.json()["message"] == "Ok":
+        json_data = response.json()["data"]
+        return redirect(json_data["url"])
+    else:
+        pass
 
 @app.route("/invoice", methods=["POST"])
 def invoice():
-    data = request.form.to_dict()
-    data["amount"] = format(float(data["amount"]),".2f")
+    amount = request.form.get("amount")
+    description = request.form.get("description")
+    currency = request.form.get("currency")
 
-    params = data.copy()
-
-    sign = generate_sign(secret_key, shop_id=shop_id, amount=data["amount"],
-        currency=data["currency"], shop_order_id="1", payway=payway)
-
-    params["sign"] = sign
-    params["payway"] = payway
-    params["shop_id"] = shop_id
-    params["shop_order_id"] = "1"
+    amount = format(float(amount),".2f")
+    sign = generate_sign(secret_key, amount=amount, currency=currency,
+        shop_id=shop_id, shop_order_id="1", payway=payway)
 
     headers = {"Content-Type": "application/json"}
-    # payload = {
-    #     "currency": data["currency"],
-    #     "payway": payway,
-    #     "amount": data["amount"],
-    #     "shop_id": shop_id,
-    #     "shop_order_id": "1",
-    #     "description": data["description"],
-    #     "sign": sign
-    # }
+    payload = {
+        "shop_id": shop_id,
+        "amount": amount,
+        "currency": currency,
+        "sign": sign,
+        "shop_order_id": "1",
+        "description": description,
+        "payway": payway
+    }
+
     session = requests.Session()
-    response = session.post("https://core.piastrix.com/invoice/create", headers=headers, data=json.dumps(params))
+    response = session.post("https://core.piastrix.com/invoice/create", headers=headers, data=json.dumps(payload))
+
     print(response.json())
-    json_data = response.json()["data"]
-    inner_data = json_data["data"]
-    # return render_template("invoice.html", url=json_data["url"], ac_account_email=inner_data["ac_account_email"],
-    #     ac_amount=inner_data["ac_amount"], ac_currency=inner_data["ac_currency"], ac_order_id=inner_data["ac_order_id"],
-    #     ac_sci_name=inner_data["ac_sci_name"])
-    return render_template("invoice.html", url=json_data["url"], method=json_data["method"], params=inner_data)
+    if response.json()["message"] == "Ok":
+        json_data = response.json()["data"]
+        inner_data = json_data["data"]
+
+        return render_template("invoice.html", url=json_data["url"], method=json_data["method"], params=inner_data)
+    else:
+        pass
